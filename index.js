f8f5fa78c547c1b03d9b0d5e8aacbc051d7bba99
defragmuhammad <defrag.muhammad10@gmail.com>
2022-09-16 18:01:45 +0700

    ini merupakan pop xample2

--- index.js
+++ index.js
@@ -3,4 +3,8 @@ console.log("halo, defrag");
 //memberi tahu file apa yng kita akan commit  perubahan nya
 // gitt add <nama file>
 // gitt add .
-
+//melihat apa saja yang berubah
+// git status
+// melakukan commit
+// git commit -m "massage nya apa?"
+// cara menyimpan file kita ke githap yaitu membuat repo di github terlebih dahulu
